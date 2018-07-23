31b470710a0e6eaa4fe394a1b6211349a89bb4f9
meetingmeeting <contendera@gmail.com>
2018-07-23 11:03:21 -0700

    built kotlin project working in new activity

---
+++ app/src/main/java/com/kotlin/albertan/albertkotlin/Mysecond_activity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.albertan.albertkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Mysecond_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mysecond_activity)
+
+    }
+}
